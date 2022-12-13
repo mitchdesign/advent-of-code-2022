546958cf909252ec89f020034c976d92328a48fa
Michael van der Griendt <griendt@kyos.com>
2022-12-13 14:28:12 +0100

    add comments

--- app/Day2.php
+++ app/Day2.php
@@ -5,31 +5,31 @@ namespace App;
 class Day2 extends Day
 {
     private array $scores = [
-        'AX' => 3 + 1,
-        'AY' => 6 + 2,
-        'AZ' => 0 + 3,
+        'AX' => 3 + 1,  // rock-rock = draw
+        'AY' => 6 + 2,  // rock-paper = win
+        'AZ' => 0 + 3,  // rock-scissors = loose
 
-        'BX' => 0 + 1,
-        'BY' => 3 + 2,
-        'BZ' => 6 + 3,
+        'BX' => 0 + 1,  // paper-rock = loose
+        'BY' => 3 + 2,  // paper-paper = draw
+        'BZ' => 6 + 3,  // paper-scissors = win
 
-        'CX' => 6 + 1,
-        'CY' => 0 + 2,
-        'CZ' => 3 + 3,
+        'CX' => 6 + 1,  // scissors-rock = win
+        'CY' => 0 + 2,  // scissors-paper = loose
+        'CZ' => 3 + 3,  // scissors-scissors = draw
     ];
 
     private array $outcomes = [
-        'AX' => 0 + 3,
-        'AY' => 3 + 1,
-        'AZ' => 6 + 2,
+        'AX' => 0 + 3,  // rock, to loose: scissors
+        'AY' => 3 + 1,  // rock, to draw: rock
+        'AZ' => 6 + 2,  // rock, to win: paper
 
-        'BX' => 0 + 1,
-        'BY' => 3 + 2,
-        'BZ' => 6 + 3,
+        'BX' => 0 + 1,  // paper, to loose: rock
+        'BY' => 3 + 2,  // paper, to draw: paper
+        'BZ' => 6 + 3,  // paper, to win: scissors
 
-        'CX' => 0 + 2,
-        'CY' => 3 + 3,
-        'CZ' => 6 + 1,
+        'CX' => 0 + 2,  // scissors, to loose: paper
+        'CY' => 3 + 3,  // scissors, to draw: scissors
+        'CZ' => 6 + 1,  // scissors, to win: rock
     ];
 
     public function result1()

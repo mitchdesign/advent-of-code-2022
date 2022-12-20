8f0d65917632c4d7a46592a42837d342d8f7d325
Michael van der Griendt <griendt@kyos.com>
2022-12-20 22:03:23 +0100

    init day 9

---
+++ app/Day9.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+class Day9 extends Day
+{
+    public static string $title = 'Rope Bridge';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function result1(): int
+    {
+        return 1;
+    }
+
+    public function result2(): int
+    {
+        return 1;
+    }
+}

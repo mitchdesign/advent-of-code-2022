badaf1b85862dd2b3f9c5b77951b476a20404c1c
Michael van der Griendt <griendt@kyos.com>
2022-12-22 14:21:35 +0100

    modify for day 9 puzzle 2

--- app/Day9.php
+++ app/Day9.php
@@ -9,6 +9,7 @@ class Day9 extends Day
 {
     public static string $title = 'Rope Bridge';
 
+    private Collection $rope;
     private Position $head;
     private Position $tail;
 
@@ -19,27 +20,27 @@ class Day9 extends Day
     {
         parent::__construct();
 
-        $this->head = new Position;
-        $this->tail = new Position;
-
         $this->visBounds = ['x' => [-10, 10], 'y' => [-10, 10]];
 
         $this->tailPositionsVisited = collect();
     }
 
+    private function initRope(int $length)
+    {
+        $this->rope = Collection::times($length, fn() => new Position);
+
+        for ($i = 1; $i < $this->rope->count(); $i++) {
+            $this->rope->get($i)->setFollows($this->rope->get($i - 1));
+        }
+
+        $this->head = $this->rope->first();
+        $this->tail = $this->rope->last();
+    }
+
     public function result1(): int
     {
-        str($this->input)->trim()
-            ->explode("\n")
-            ->each(function ($move) {
-                [$direction, $amount] = explode(' ', $move);
-                for ($i = 0; $i < $amount; $i++) {
-                    $this->head->move($direction);
-                    $this->tail->follow($this->head);
-                    $this->logTailPosition($this->tail);
-                }
-            });
-
+        $this->initRope(2);
+        $this->followMoves();
         $this->visualize();
 
         return $this->tailPositionsVisited->unique()->count();
@@ -47,7 +48,25 @@ class Day9 extends Day
 
     public function result2(): int
     {
-        return 1;
+        $this->initRope(10);
+        $this->followMoves();
+        $this->visualize();
+
+        return $this->tailPositionsVisited->unique()->count();
+    }
+
+    private function followMoves(): void
+    {
+        str($this->input)->trim()
+            ->explode("\n")
+            ->each(function ($move) {
+                [$direction, $amount] = explode(' ', $move);
+                for ($i = 0; $i < $amount; $i++) {
+                    $this->head->move($direction);
+                    $this->rope->each(fn ($node) => $node->follow());
+                    $this->logTailPosition($this->tail);
+                }
+            });
     }
 
     private function logTailPosition(Position $tail): void
@@ -57,10 +76,10 @@ class Day9 extends Day
 
         $this->tailPositionsVisited->push((string) $tail);
 
-        $this->visBounds['x'][0] = min($this->visBounds['x'][0], $x - 5);
-        $this->visBounds['x'][1] = max($this->visBounds['x'][1], $x + 5);
-        $this->visBounds['y'][0] = min($this->visBounds['y'][0], $y - 5);
-        $this->visBounds['y'][1] = max($this->visBounds['y'][1], $y + 5);
+        $this->visBounds['x'][0] = min($this->visBounds['x'][0], $x - 10);
+        $this->visBounds['x'][1] = max($this->visBounds['x'][1], $x + 10);
+        $this->visBounds['y'][0] = min($this->visBounds['y'][0], $y - 10);
+        $this->visBounds['y'][1] = max($this->visBounds['y'][1], $y + 10);
     }
 
     private function visualize()
@@ -73,8 +92,13 @@ class Day9 extends Day
            $vis[$y][$x] = '#';
         });
 
-        $vis[$this->tail->y][$this->tail->x] = 'T';
-        $vis[$this->head->y][$this->head->x] = 'H';
+        $this->rope->reverse()->each(function ($node, $key) use (&$vis) {
+            $symbol = match ($key) {
+                0 => 'H',
+                default => $key
+            };
+            $vis[$node->y][$node->x] = $symbol;
+        });
 
         echo '<pre style="font-size: 0.5rem;">' . join("\n", array_map(fn ($row) => join('', $row), $vis)) . '</pre>';
     }

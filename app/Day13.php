4cc848be36abc50dcf5bd7ddb2ccf77071c73bdb
Michael van der Griendt <griendt@kyos.com>
2023-01-01 19:12:44 +0100

    add day 13

--- app/Day13.php
+++ app/Day13.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
 
 class Day13 extends Day
@@ -15,7 +16,7 @@ class Day13 extends Day
         parent::__construct();
 
         // get the inputs as php arrays in arrays of [input1, input2], with 1-based index
-        $this->inputs = str($this->input)
+        $this->inputs = str(trim($this->input))
             ->explode("\n\n")
             ->mapWithKeys(fn ($group, $index) => [$index + 1 => str($group)
                 ->explode("\n")
@@ -25,20 +26,51 @@ class Day13 extends Day
 
     public function result1(): int
     {
-        // filter the inputs using a sort function. it outputs -1 if input 1 < input 2. 0 if equal.
-        // in both cases the input is in the right order, and so we keep it, to sum the indexes to get the answer.
-        return $this->inputs->filter(fn($input) => in_array($this->sort(...$input), [-1, 0]))
+        // filter the inputs using a sort function.
+        // if sort function returns <1 or 0 then the inputs are in the right order.
+        return $this->inputs->filter(fn($input) => $this->sort(...$input) <= 0)
             ->keys()
             ->sum();
     }
 
     public function result2(): int
     {
-        return 1;
+        /* @var \Illuminate\Support\Collection $sorted */
+        $sorted = $this->inputs->flatten(1) // put the duos of inputs into 1 collection
+            ->add([[2]])
+            ->add([[6]])
+            ->sort([$this, 'sort'])
+            ->prepend('') // the puzzle says items are 1-indexed, add dummy in front
+            ->values();
+
+        $index1 = $sorted->search([[2]]);
+        $index2 = $sorted->search([[6]]);
+
+        return $index1 * $index2;
     }
 
-    public function sort(array $input1, array $input2): int
+    // sorting callback function.
+    // it returns <1 if input 1 smaller than input2, >1 if larger, 0 if equal
+    public function sort(int|array $input1, int|array $input2): int
     {
-        return 0;
+        if (is_int($input1) && is_int($input2)) {
+            return $input1 - $input2;
+        }
+
+        $input1 = Arr::wrap($input1);
+        $input2 = Arr::wrap($input2);
+
+        $i = 0;
+        while (isset($input1[$i]) && isset($input2[$i])) {
+            $sort = $this->sort($input1[$i], $input2[$i]);
+            if ($sort !== 0) {
+                return $sort;
+            }
+            $i++;
+        }
+
+        // end of array. if length of input1 is longer, then it is left is larger (return 1)
+        // so we return the comparison of the 2 lengths.
+        return count($input1) - count($input2);
     }
 }
